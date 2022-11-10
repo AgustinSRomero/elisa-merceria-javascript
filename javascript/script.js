79132c19e9d39ff0e9e10cc06eba054ea6f50732
Agustin <romero.s.agustin@outlook.com>
2022-11-10 20:12:50 -0300

    Changes v1
    agregado página compra, precio final, arreglado todo el carrito. Falta proceso de finalizar compra y métodos de búsqueda

--- javascript/script.js
+++ javascript/script.js
@@ -24,6 +24,7 @@ class producto {
         this.nombre = nombre         
         this.precio = precio 
         this.stock = stock
+        this.cantidad = 1
         this.tipo = tipo
         this.img = img
         this.altImg = altImg
@@ -34,13 +35,21 @@ let carrito = []
 
 const contenedorProductos = document.querySelector('#products-container')
 
-const carritoNotificacion = document.getElementsByClassName('carrito-notification')
+const carritoNotificacion = document.querySelector('#carrito-notification')
 
+const vaciarCarrito = document.querySelector('#vaciar-carrito')
+
+const precioTotal = document.querySelector('#precio-total')
+
+const finalizarCompra = document.querySelector('#finalizar-compra')
+
+// Me fijo si hay un carrito guardado en el local storage y lo renderizo
 document.addEventListener('DOMContentLoaded', () => {
     carrito = JSON.parse(localStorage.getItem('carrito')) || []
     mostrarCarrito()
 })
 
+// Renderizo todos los productos de la tienda
 productos.forEach((prod) => {
     const {id, nombre, precio, stock, img, altImg} = prod
     const div = document.createElement('div')
@@ -50,23 +59,53 @@ productos.forEach((prod) => {
         <img src="${img}" alt="${altImg}">
         <p>${nombre}</p> 
         <p>$${precio}</p>
-        <input id="quantity" class="input-quantity" type="number" placeholder="1" min="1" max="${stock}">
-        <button onclick="agregarProducto(${id})" class="button-catalogo">Añadir al Carrito</button>
+        <input id="quantity${id}" class="input-quantity" type="number" value="1" min="1" max="${stock}">
+        <button onclick="agregarProducto(${id}, ${precio}, cantidadProducto(${id}))" class="button-catalogo">Añadir al Carrito</button>
     `
+    //En el boton tengo el evento onclick que activa la funcion que agrega al carrito con dos parametros, producto id y cantidad producto
     contenedorProductos.appendChild(div)
 })
 
-function agregarProducto(id){
-    console.log(valor)
+//Funcion para obtener el valor de cantidad que ingreso el usuario
+function cantidadProducto(id){
+    let identificacion = "#quantity" + id
+    let input = document.querySelector(identificacion)
+    let quantity = Number(input.value)
+    return quantity
+}
+
+//Funcion que agrega los items al carrito
+function agregarProducto(id, precio, cantidad){
     const item = productos.find((producto) => producto.id === id)
-    carrito.push(item)
+    const validar = carrito.some((prod) => prod.id === id)
+
+    if(validar){
+        const prodCarrito = carrito.find((prod) => prod.id === id)
+        prodCarrito.cantidad = prodCarrito.cantidad + cantidad
+        //Calculo precio cuando tengo productos en el carrito
+        prodCarrito.precio = prodCarrito.precio + (precio * cantidad)
+        
+    }else{
+        //Seteo cantidad y precio
+        item.cantidad = cantidad
+        item.precio = precio * cantidad
+        
+        carrito.push(item)
+    }
+    
     mostrarCarrito() 
 }
 
+// Funcion que renderiza el carrito
 const mostrarCarrito = () => {
     const carritoContainer = document.querySelector('.modal-body')
     
     carritoContainer.innerHTML = ''
+    
+    if(carrito.length === 0){
+        carritoContainer.innerHTML = `<p class="text-center">Todavia no agregaste nada a tu carrito</p>`
+    }
+    
     carrito.forEach(prod => {
         const {id, nombre, precio, cantidad, img, altImg} = prod
 
@@ -77,22 +116,60 @@ const mostrarCarrito = () => {
             </div>
             <div>
                 <p>${nombre}</p> 
-                <p>$${precio}</p>
-                <input class="input-quantity" type="number" value="${cantidad}">
+                <p>Subtotal: $${precio}</p>
+                <p>Cantidad: ${cantidad}</p>
             </div>
-            <button onclick="eliminarProducto(${id})" type="button" class="btn-close" aria-label="Close"></button>
+            <button onclick="eliminarProducto(${id})" type="button" id="quantity-carrito${id}" class="btn-close" aria-label="Close"></button>
         </section>
         `
     });
 
+    carrito.length != 0 ? carritoNotificacion.textContent = carrito.length : carritoNotificacion.textContent = ""
+    
     guardarStorage()
+    calculoFinal()
 }
 
+//Funcion que elimina un producto del carrito cuando se apreta la x
 function eliminarProducto (id){
     carrito = carrito.filter((prod) => prod.id !== id)
     mostrarCarrito()
 }
 
+//Funcion para vaciar todo el carrito
+vaciarCarrito.addEventListener('click', () => {
+    carrito = []
+    mostrarCarrito()
+})
+
+//Funcion que guarda el carrito en el storage
 function guardarStorage(){
     localStorage.setItem('carrito', JSON.stringify(carrito))
 }
+
+//Calculo precio final
+function calculoFinal(){
+if (carrito.length !== 0) {
+        let precioFinal = carrito.reduce((acc, item) => {
+            return acc += Number(item.precio)}, 0)
+        precioTotal.innerText = `Precio total: $${precioFinal}`
+    }else{
+        precioTotal.innerText = "Precio total: $0"
+    }
+}
+
+//Funcionalidad del boton finalizar compra
+finalizarCompra.addEventListener('click', () => {
+    if (carrito.length !== 0) {
+        location.href = "../pages/compra.html"
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Carrito Vacío',
+            text: 'Por favor, elija algun producto para poder continuar',
+            color: '#596f91',
+            background: '#EFEBE4',
+            confirmButtonColor: '#918367d2'
+        })
+    }
+})
